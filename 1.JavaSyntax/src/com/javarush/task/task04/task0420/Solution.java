5308690d22e5892eaec656a104e06f14721b8a0d
N1Kit <n9052233291@ya.ru>
2018-12-21 06:04:43 +0300

    I finished task0421 (home)

--- 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -4,23 +4,29 @@ package com.javarush.task.task04.task0420;
 Сортировка трех чисел
 */
 
-import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
         Scanner scanner = new Scanner(System.in);
-        int [] nums = new int[3];
+        int[] nums = new int[3];
+
         for (int i = 0; i < nums.length; i++) {
-           nums[i] = Integer.parseInt(scanner.nextLine());
+            nums[i] = Integer.parseInt(scanner.nextLine());
         }
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i-1]<=nums[i]){
-                nums[i-1]=nums[i-1]+nums[i]-(nums[i]=nums[i-1]);    //  a=a+b-(b=a);
+
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j =  0; j < nums.length-i-1; j++) {
+                if (nums[j] <= nums[j+1]) {
+                    nums[j] = nums[j] + nums[j+1] - (nums[j+1] = nums[j]);    //  a=a+b-(b=a);
+                }
             }
         }
-            System.out.println(Arrays.toString(nums));
+
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+        //System.out.println(Arrays.toString(nums));
     }
 }
