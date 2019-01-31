3a4a717432bcc1e289aeaceb1a90bdbccebdff56
N1Kit <n9052233291@ya.ru>
2019-01-31 11:29:50 +0300

    The last decided task is task 0520

--- 1.JavaSyntax/src/com/javarush/task/task05/task0520/Rectangle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0520/Rectangle.java
@@ -4,9 +4,35 @@ package com.javarush.task.task05.task0520;
 Создать класс прямоугольник (Rectangle)
 */
 
-
 public class Rectangle {
     //напишите тут ваш код
+    int top, left, width, height;
+
+    public Rectangle(int top, int left, int width, int height){
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(int top, int left){
+        this.top = top;
+        this.left = left;
+        this.height = this.width = 0;
+    }
+
+    public Rectangle(int top, int left, int width){
+        this.top = top;
+        this.left = left;
+        this.height = this.width = width;
+    }
+
+    public Rectangle(Rectangle rectangle){
+        this.top = rectangle.top;
+        this.left = rectangle.left;
+        this.width = rectangle.width;
+        this.height = rectangle.height;
+    }
 
     public static void main(String[] args) {
 
