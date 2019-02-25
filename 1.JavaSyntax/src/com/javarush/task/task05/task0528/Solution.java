9b1f58f671b8e47d0d3614d7c655e20ce8612725
N1Kit <n9052233291@ya.ru>
2019-02-25 17:25:21 +0300

    Task 0528 edited (work)

--- 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
         //Date date = new Date();
-        System.out.printf("%1$s %2$td %2$tm %2$tY", "", new Date());
+        System.out.printf("%1$s %2$td %2$tm %2$tY", "Текущая дата:", new Date());
         //System.out.printf("%s %te %<tB %<tY", "", date);
     }
 }
