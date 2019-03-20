0a441f3720c5cd3127e7f1466b1c94c56830d0ae
N1Kit <n9052233291@ya.ru>
2019-03-20 06:29:16 +0300

    Task0716 in progress (home)

--- 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task07.task0716;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /* 
 Р или Л
@@ -12,6 +13,13 @@ public class Solution {
         list.add("роза"); // 0
         list.add("лоза"); // 1
         list.add("лира"); // 2
+        list.add("поза");
+        list.add("рана");
+        list.add("кожа");
+        list.add("лажа");
+        list.add("лама");
+        list.add("пола");
+
         list = fix(list);
 
         for (String s : list) {
@@ -21,6 +29,17 @@ public class Solution {
 
     public static ArrayList<String> fix(ArrayList<String> list) {
         //напишите тут ваш код
-        return null;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).contains("р") && list.get(i).contains("л"))
+                continue;
+            else if (list.get(i).contains("р")) {
+                list.remove(i);
+                i--;
+            } else if (list.get(i).contains("л")) {
+                list.add(i+1, list.get(i));
+                i+=2;
+            }
+        }
+        return list;
     }
 }
