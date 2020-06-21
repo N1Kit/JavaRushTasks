43340dd12077f45d2b3f701369f8729bc4431837
Dan11l <noname.spb@mail.ru>
2020-06-21 11:25:17 +0300

    Merge branch 'master' of D:\JJD\JavaRushTasks with conflicts.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -10,25 +10,25 @@ import java.util.List;
 Cамая длинная последовательность
 */
 public class Solution {
-  public static void main(String[] args) throws IOException {
-    //напишите тут ваш код
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    List<Integer> list = new ArrayList<>();
-    for (int i = 0; i < 10; i++) {
-      list.add(Integer.parseInt(reader.readLine()));
-    }
-    int maxlen = 1;
-    int curlen = 1;
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<>();
 
-    for (int i = 1; i < list.size(); i++) {
-      if (list.get(i).equals(list.get(i - 1))) {
-        curlen++;
-        if (curlen > maxlen) {
-          maxlen = curlen;
+        for (int i = 0; i < 10; i++) {
+            list.add(Integer.parseInt(reader.readLine()));
         }
-      } else
-        curlen = 1;
+        int temp = 1;
+        int count = 1;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).equals(list.get(i - 1))) {
+                temp++;
+                if (temp > 1)
+                    count = temp;
+            }
+            else
+                temp = 1;
+        }
+        System.out.println(count);
     }
-    System.out.println(maxlen);
-  }
 }
